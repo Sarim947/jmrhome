e2eb048a2285af6b55f6d459c25a87bc12af2561
Sarim947 <740351598liu@gmail.com>
2026-06-06 16:28:45 +0800

    inquiry content

---
+++ app/inquiry/page.jsx
@@ -0,0 +1,12 @@
+import SiteShell from "@/components/SiteShell";
+
+export default function InquiryPage() {
+  return (
+    <SiteShell>
+      <main className="container section">
+        <h1>Project Inquiry</h1>
+        <p>Tell us about your project and our team will contact you shortly.</p>
+      </main>
+    </SiteShell>
+  );
+}
