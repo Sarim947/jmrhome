795182db2bc9822a711e44358cadf6feacca2db4
Sarim947 <740351598liu@gmail.com>
2026-06-06 16:54:24 +0800

    add inquiry

--- app/inquiry/page.jsx
+++ app/inquiry/page.jsx
@@ -1,12 +1,8 @@
-import SiteShell from "@/components/SiteShell";
-
 export default function InquiryPage() {
   return (
-    <SiteShell>
-      <main className="container section">
-        <h1>Project Inquiry</h1>
-        <p>Tell us about your project and our team will contact you shortly.</p>
-      </main>
-    </SiteShell>
+    <main style={{ padding: "80px" }}>
+      <h1>Project Inquiry</h1>
+      <p>Tell us about your project and our team will contact you shortly.</p>
+    </main>
   );
 }
